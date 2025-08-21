dc66c4d92efe221af8e31f3b0d36efaabcc19d93
Livesh Kumar <129591578+SOFTGAMESTUDIO@users.noreply.github.com>
2025-08-21 18:57:08 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebas
 import { getFirestore} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxZD9v4rH5e3VBUE_37s_M5mzeJD-Zz9k",
-  authDomain: "apk-store-90e52.firebaseapp.com",
-  projectId: "apk-store-90e52",
-  storageBucket: "apk-store-90e52.firebasestorage.app",
-  messagingSenderId: "352993687207",
-  appId: "1:352993687207:web:a5a20b64fa7aeaf8040d82",
-  measurementId: "G-XTKPXSKRW5"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
